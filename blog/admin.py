403e8343013dd6523206fee52a8806a5ea543865
serhiibezuhlyi <bezuglyjs037@gmail.com>
2025-12-13 20:54:15 +0200

    admin panel

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from blog.models import Category, Article, Comment, Tag
+
 # Register your models here.
+admin.site.register(Category)
+admin.site.register(Article)
+admin.site.register(Comment)
+admin.site.register(Tag)
