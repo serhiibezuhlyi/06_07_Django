acdd7ca71241949c2c6a5b4118b6fe7fccce76d8
serhiibezuhlyi <bezuglyjs037@gmail.com>
2025-12-13 20:40:24 +0200

    nginx

--- main/settings.py
+++ main/settings.py
@@ -126,8 +126,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'static'
+STATIC_URL = "/static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
