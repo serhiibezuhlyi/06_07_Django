3e7592356a2237046a8f9e692ad887b0114e26fe
serhiibezuhlyi <bezuglyjs037@gmail.com>
2025-12-14 13:01:33 +0200

    article details + comments

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path("", views.Index.as_view(), name="index"),
     path("all_articles/", views.AllArticles.as_view(), name="all_articles"),
+    path("article/<int:pk>", views.ArticleDetail.as_view(), name="article_detail"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 from blog.models import Article
 
@@ -18,3 +18,6 @@ class Index(ListView):
 class AllArticles(ListView):
     model = Article
     template_name = "blog/all_articles.html"
+
+class ArticleDetail(DetailView):
+    model = Article
